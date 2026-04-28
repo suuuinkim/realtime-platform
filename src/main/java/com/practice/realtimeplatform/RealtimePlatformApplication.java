b7e418f5cd87f69cff83250fc26ff436384c008b
sikim <suchae7@naver.com>
2026-04-29 08:41:04 +0900

    init : spring boot project

---
+++ src/main/java/com/practice/realtimeplatform/RealtimePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.practice.realtimeplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealtimePlatformApplication.class, args);
+    }
+
+}
